8ec5396331b12870610ae640f4223d21c4057a8b
EasytolearnWebDeveloper <easytolearnwebdev@gmail.com>
2022-07-02 04:00:51 +0530

    js

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 
-document.getElementById('answer').readOnly = true; //set this attribute in Html file
-let screen = document.getElementById('answer');
+document.getElementById('result').readOnly = true; 
+let screen = document.getElementById('result');
 buttons = document.querySelectorAll('button');
 let screenValue = '';
 for (item of buttons) {
     item.addEventListener('click', (e) => {
-        // console.log(buttonText, "has been pressed");
         buttonText = e.target.innerText;
         if (buttonText == 'X') {
             buttonText = '*';
@@ -27,51 +26,7 @@ for (item of buttons) {
     })
 }
 
-document.addEventListener("keydown", function(event) {
-    console.log(event.which);
-    if(event.shiftKey==57){
-        event.key = '(';
-    }
-    else if(event.shiftKey==48){
-        event.key = ')';
-    }
-    else if(event.shiftKey==53){
-        event.key = '%';
-    }
-    if(event.keyCode==88){
-        screenValue += '*';
-        screen.value = screenValue;
-    }
-    if(event.key<=9 || event.key=='+' || event.key=='-' || event.key=='*' || event.key=='.' || event.key=='/' || event.key=='%' || event.key=='(' || event.key==')'){
-        screenValue += event.key;
-        screen.value = screenValue;
-    }
-    if(event.keyCode == 13 || event.keyCode == 187)
-    {
-        screen.value = eval(screenValue);
-    }
-    else if(event.keyCode == 46){
-        screenValue = "";
-        screen.value = screenValue;
-        console.clear();
-    }
-    else if(event.keyCode == 8){
-        screenValue = screenValue.slice(0, -1);
-        screen.value = screenValue;
-    }
-    else if(event.keyCode == 67){
-        screenValue = "";
-        screen.value = screenValue;
-        console.clear();
-    }
-    else if(event.keyCode == 82){
-        location.reload();
-    }
-  })
 
   window.onerror = function(){
       alert("Invaild Keyword");
-      screenValue = "";
-      screen.value = screenValue;
-      console.clear();
   }
